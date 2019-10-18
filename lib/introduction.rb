9903e440421d4afd3567ea3d4e19ea024683ef14
Maika Kailani <queenmab051@gmail.com>
2019-10-18 03:51:55 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -13,7 +13,7 @@ end
 
 introduction("Maika", "Ruby")
 
-def introduction(name, language = "GO")
+def introduction_with_language_optional(name, language = "GO")
   puts "Hi my name is #{name} and I am learning to program in #{language}."
 end
  
